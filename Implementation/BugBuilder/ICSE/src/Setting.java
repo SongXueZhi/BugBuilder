f8afaf7e6358227cd7b9b30a5edb6f888d492713
SongXueZhi <sxzdhu@gmail.com>
2023-04-11 11:00:31 +0800

    refact to maven project

--- Implementation/BugBuilder/ICSE/src/Setting.java
+++
@@ -1,4 +0,0 @@
-public class Setting {
-    //initialize Defects4J executable path
-    public static String defects4jPath="";//""/users/yanjiejiang/defects4j/framework/bin";
-}
---
+++ src/main/java/Setting.java
@@ -0,0 +1,4 @@
+public class Setting {
+    //initialize Defects4J executable path
+    public static String defects4jPath="/home/sxz/data/defects4j/framework/bin";//""/users/yanjiejiang/defects4j/framework/bin";
+}
